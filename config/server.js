6b4b21248a6c9094d399d1e9c3b43decc4bca981
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2022-11-03 13:34:53 +0300

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array(
+      "6K08ZP8CmPm/XzVgXX+Gcw==, PzR7MTwMCybk8Ap2/rza1w==,vr7olbZjEu4aq7GQ0Mv/yQ==,v33B3zqDwMdOQQWf96isGA=="
+    ),
   },
 });
